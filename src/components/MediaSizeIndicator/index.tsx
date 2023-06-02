da1b2f3c9e4d212180952a42983c8f047303fa9a
whyleonardo <pleasefloyd73@gmail.com>
2023-06-02 20:11:48 -0300

    chore(deps): remove eslint order import plugin

--- src/components/MediaSizeIndicator/index.tsx
+++ src/components/MediaSizeIndicator/index.tsx
@@ -5,7 +5,7 @@ export const MediaSizeIndicator = () => {
 
 	return (
 		<div className="fixed bottom-1 left-1 z-50 flex items-center justify-center gap-2 rounded-full bg-gray-800 p-3 font-mono text-xs font-bold text-white">
-			 <div className="block sm:hidden">xs</div>
+			<div className="block sm:hidden">xs</div>
 			<div className="hidden sm:block md:hidden">sm</div>
 			<div className="hidden md:block lg:hidden">md</div>
 			<div className="hidden lg:block xl:hidden">lg</div>
@@ -13,7 +13,6 @@ export const MediaSizeIndicator = () => {
 			<div className="hidden 2xl:block">2xl</div>
 
 			<Icons.tailwind className="h-4 w-4" />
-
 		</div>
 	)
 }
