49fcb386a0ac76d3b57294d29cfb200ebf56df06
Christian Leonardo <pleasefloyd73@gmail.com>
2023-06-27 14:11:07 -0300

    feat: create vitest.config.ts file

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/__tests__/setup.ts',
+		include: ['**/*.test.tsx']
+	},
+
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
+})
