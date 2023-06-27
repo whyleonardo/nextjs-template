839d579894622e52979e95be590780f848cff459
Christian Leonardo <pleasefloyd73@gmail.com>
2023-06-27 14:13:10 -0300

    chore: import GitHubLink component into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { GithubLink } from '@/components/Buttons/GithubLink'
 import { ThemeToggle } from '@/components/ThemeToggle'
 
 export default function IndexPage() {
 	return (
 		<div className="relative flex min-h-screen items-center justify-center">
+			<GithubLink />
 			<ThemeToggle />
 
 			<span className="flex flex-wrap items-center justify-center gap-1 px-4 text-5xl font-semibold tracking-tighter">
