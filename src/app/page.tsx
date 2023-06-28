bb5414d9ecbe9234ed4c02e7a886295fb71dfed1
Christian Leonardo <pleasefloyd73@gmail.com>
2023-06-27 22:12:48 -0300

    refactor: update SocialLink import and change page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
-import { GithubLink } from '@/components/Buttons/GithubLink'
-import { ThemeToggle } from '@/components/ThemeToggle'
+import { SocialLinks } from '@/components/Buttons/SocialLinks'
+import { ThemeToggle } from '@/components/Buttons/ThemeToggle'
 
 export default function IndexPage() {
 	return (
-		<div className="relative flex min-h-screen items-center justify-center">
-			<GithubLink />
-			<ThemeToggle />
+		<div className="h-screen">
+			<header className="flex h-14 w-full items-center justify-end px-4">
+				<div className="flex items-center gap-2 ">
+					<SocialLinks.Github />
+					<SocialLinks.Linkedin />
+					<ThemeToggle />
+				</div>
+			</header>
 
-			<span className="flex flex-wrap items-center justify-center gap-1 px-4 text-5xl font-semibold tracking-tighter">
-				Next.js +
-				<span className="bg-gradient-to-br from-blue-700 to-blue-600 bg-clip-text text-transparent dark:from-blue-500 dark:to-blue-400">
-					Tailwind{' '}
-					<span className="text-neutral-950 dark:text-neutral-50">+&nbsp;</span>
+			<div className="flex h-[calc(100%-3.5rem)] items-center justify-center">
+				<span className="flex flex-wrap items-center justify-center gap-1 px-4 text-5xl font-semibold tracking-tighter">
+					Next.js +
+					<span className="bg-gradient-to-br from-blue-700 to-blue-600 bg-clip-text text-transparent dark:from-blue-500 dark:to-blue-400">
+						Tailwind <span>+&nbsp;</span>
+					</span>
+					shadcn-ui ❤️
 				</span>
-				shadcn-ui ❤️
-			</span>
+			</div>
 		</div>
 	)
 }
