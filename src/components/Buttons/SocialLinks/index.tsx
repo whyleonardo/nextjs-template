028fe310561d0b366440398c91313e5a57eb56a6
Christian Leonardo <pleasefloyd73@gmail.com>
2023-06-27 22:07:59 -0300

    feat(SocialLinks): create social links compound

---
+++ src/components/Buttons/SocialLinks/index.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+
+import { buttonVariants } from '@/components/ui/button'
+
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
+
+const Github = () => {
+	return (
+		<Link
+			className={buttonVariants({
+				variant: 'ghost',
+				size: 'icon'
+			})}
+			href="https://github.com/whyleonardo"
+			data-testid="github-link"
+			target="_blank"
+		>
+			<GitHubLogoIcon className="icon" />
+			<span className="sr-only">Github Link</span>
+		</Link>
+	)
+}
+
+const Linkedin = () => {
+	return (
+		<Link
+			className={buttonVariants({
+				variant: 'ghost',
+				size: 'icon'
+			})}
+			href="https://linkedin.com/in/christianlsb"
+			data-testid="github-link"
+			target="_blank"
+		>
+			<LinkedInLogoIcon className="icon" />
+			<span className="sr-only">Github Link</span>
+		</Link>
+	)
+}
+
+export const SocialLinks = { Github, Linkedin }
