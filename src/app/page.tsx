2d8b72334795bf9a6cc9d958c50c7dc2baeef6d7
Christian Leonardo <pleasefloyd73@gmail.com>
2023-06-28 11:31:29 -0300

    refactor: adjust some responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from '@/components/Buttons/ThemeToggle'
 export default function IndexPage() {
 	return (
 		<div className="h-screen">
-			<header className="container flex h-14 items-center justify-end">
+			<header className="container flex h-14 items-center justify-end pt-2">
 				<div className="flex items-center gap-2 ">
 					<SocialLinks.Github />
 					<SocialLinks.Linkedin />
@@ -13,7 +13,7 @@ export default function IndexPage() {
 			</header>
 
 			<div className="flex h-[calc(100%-3.5rem)] items-center justify-center">
-				<span className="flex flex-wrap items-center justify-center gap-1 px-4 text-5xl font-semibold tracking-tighter">
+				<span className="flex flex-wrap items-center justify-center gap-1 px-4 text-4xl font-semibold tracking-tighter sm:text-5xl">
 					Next.js +
 					<span className="bg-gradient-to-br from-blue-700 to-blue-600 bg-clip-text text-transparent dark:from-blue-500 dark:to-blue-400">
 						Tailwind <span>+&nbsp;</span>
