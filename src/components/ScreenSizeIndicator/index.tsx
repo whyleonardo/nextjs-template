ca61069d4f35937e6e9ae608d1961ea99870e200
Christian Leonardo <pleasefloyd73@gmail.com>
2023-06-28 11:10:43 -0300

    refactor: change colors to custom css variables

--- src/components/ScreenSizeIndicator/index.tsx
+++ src/components/ScreenSizeIndicator/index.tsx
@@ -4,7 +4,7 @@ export const ScreenSizeIndicator = () => {
 	if (process.env.NODE_ENV === 'production') return null
 
 	return (
-		<div className="fixed bottom-1 left-1 z-50 flex items-center justify-center gap-2 rounded-full bg-neutral-900 p-3 font-mono text-xs font-bold text-neutral-50 dark:bg-neutral-100 dark:text-neutral-900">
+		<div className="bg-foreground text-background fixed bottom-1 left-1 z-50 flex items-center justify-center gap-2 rounded-full p-3 font-mono text-xs font-bold">
 			<div className="block sm:hidden">xs</div>
 			<div className="hidden sm:block md:hidden">sm</div>
 			<div className="hidden md:block lg:hidden">md</div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,21 +5,20 @@ import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-400 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-neutral-800',
+	'focus-visible:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50',
 	{
 		variants: {
 			variant: {
 				default:
-					'bg-neutral-900 text-neutral-50 shadow hover:bg-neutral-900/90 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90',
+					'bg-primary text-primary-foreground hover:bg-primary/90 shadow',
 				destructive:
-					'bg-red-500 text-neutral-50 shadow-sm hover:bg-red-500/90 dark:bg-red-800 dark:text-red-50 dark:hover:bg-red-800/90',
+					'bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm',
 				outline:
-					'border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
+					'border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm',
 				secondary:
-					'bg-neutral-100 text-neutral-900 shadow-sm hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80',
-				ghost:
-					'hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
-				link: 'text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50'
+					'bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm',
+				ghost: 'hover:bg-accent hover:text-accent-foreground',
+				link: 'text-primary underline-offset-4 hover:underline'
 			},
 			size: {
 				default: 'h-9 px-4 py-2',
