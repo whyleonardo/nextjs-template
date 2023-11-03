4e029e8e619b7db3e14a4e1b6f9546470bf5e683
whyleonardo <pleasefloyd73@gmail.com>
2023-11-03 12:52:30 -0300

    chore: remove cal-sans font and update deps

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,12 +1,11 @@
 import { JetBrains_Mono as FontMono, Inter as FontSans } from 'next/font/google'
-import 'cal-sans'
 
 export const fontSans = FontSans({
 	subsets: ['latin'],
-	variable: '--font-sans'
+	variable: '--font-sans',
 })
 
 export const fontMono = FontMono({
 	subsets: ['latin'],
-	variable: '--font-mono'
+	variable: '--font-mono',
 })
