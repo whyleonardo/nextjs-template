21ed1a901daf989eca15c083a42ef9de25df81a3
whyleonardo <pleasefloyd73@gmail.com>
2024-01-03 16:14:58 -0300

    chore: abstract storybook config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,33 +1,29 @@
-import type { Preview } from '@storybook/react';
+import type { Preview } from '@storybook/react'
 
-import { withThemeByClassName } from '@storybook/addon-styling';
+import { withThemeByClassName } from '@storybook/addon-themes'
 
-/* TODO: update import to your tailwind styles file */
 import '../src/styles/global.css'
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+	},
+}
 
-  decorators: [
-    // Adds theme switching support.
-    // NOTE: requires setting "darkMode" to "class" in your tailwind config
-    withThemeByClassName({
-      themes: {
-        light: 'light',
-        dark: 'dark',
-      },
-			defaultTheme: 'light',
+export const decorators = [
+	withThemeByClassName({
+		themes: {
+			light: 'light',
+			dark: 'dark',
+		},
+		defaultTheme: 'light',
+	}),
+]
 
-    }),
-  ],
-};
-
-export default preview;
+export default preview
