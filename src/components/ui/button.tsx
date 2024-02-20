9b14230a8fa6718bca1e07f2aa8f03040728703a
Christian Leonardo <pleasefloyd73@gmail.com>
2024-02-20 10:41:21 -0300

    feat: add new size variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -28,6 +28,7 @@ const buttonVariants = cva(
 			size: {
 				default: "h-9 px-4 py-2",
 				sm: "h-8 rounded-md px-3 text-xs",
+				xs: "h-7 rounded-md px-2 text-xs",
 				lg: "h-10 rounded-md px-8",
 				icon: "size-9 p-2"
 			}
