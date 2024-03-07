8bdb81484b44d2a3590c3da09649aabb1d13d4f3
Christian Leonardo <pleasefloyd73@gmail.com>
2024-03-07 18:40:37 -0300

    chore: remove some unused libs and files

--- src/__tests__/sampleTest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest"
-
-describe("Sample test in /shared", () => {
-	it("true===true", () => {
-		expect(true).toBe(true)
-	})
-})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,125 +1,78 @@
-import { type Config } from 'tailwindcss'
-
-const {
-	default: flattenColorPalette,
-} = require('tailwindcss/lib/util/flattenColorPalette')
-const svgToDataUri = require('mini-svg-data-uri')
+import type { Config } from "tailwindcss"
 
 export default {
-	darkMode: ['class'],
-	content: [
-		'./pages/**/*.{ts,tsx}',
-		'./components/**/*.{ts,tsx}',
-		'./app/**/*.{ts,tsx}',
-		'./src/**/*.{ts,tsx}',
-	],
+	darkMode: ["class"],
+	content: ["./src/**/*.{ts,tsx}"],
+	prefix: "",
 	theme: {
 		container: {
 			center: true,
-			padding: '2rem',
+			padding: "2rem",
 			screens: {
-				'2xl': '1400px',
-			},
+				"2xl": "1400px"
+			}
 		},
 		extend: {
 			colors: {
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				svg: 'hsl(0 0% 63.9%)',
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
 				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))',
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))"
 				},
 				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))',
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))"
 				},
 				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))',
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))"
 				},
 				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))"
 				},
 				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))"
 				},
 				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))',
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))"
 				},
 				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))',
-				},
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))"
+				}
 			},
 			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)',
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: ['var(--font-geist-sans)'],
-				mono: ['var(--font-geist-mono)'],
-				calSans: ['var(--font-cal-sans)'],
-				poppins: ['var(--font-poppins)'],
+				sans: ["var(--font-geist-sans)"],
+				mono: ["var(--font-geist-mono)"],
+				cal: ["var(--font-cal-sans)"]
 			},
 			keyframes: {
-				'accordion-down': {
-					from: { height: '0' },
-					to: { height: 'var(--radix-accordion-content-height)' },
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" }
 				},
-				'accordion-up': {
-					from: { height: 'var(--radix-accordion-content-height)' },
-					to: { height: '0' },
-				},
-				gradient: {
-					'0%': {
-						backgroundPosition: '0%',
-					},
-					'50%': {
-						backgroundPosition: '100%',
-					},
-					'100%': {
-						backgroundPosition: '100%',
-					},
-				},
-			},
-			backgroundSize: {
-				gradient: '250%',
-			},
-			backgroundImage: {
-				'radial-gradient':
-					'radial-gradient(ellipse at center, var(--tw-gradient-stops))',
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" }
+				}
 			},
 			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out',
-				gradient: 'gradient 5s ease infinite alternate',
-			},
-		},
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out"
+			}
+		}
 	},
-	plugins: [
-		require('tailwindcss-animate'),
-		function ({ matchUtilities, theme }: { matchUtilities: any; theme: any }) {
-			matchUtilities(
-				{
-					'bg-grid': (value: string) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
-						)}")`,
-					}),
-				},
-				{
-					values: flattenColorPalette(theme('backgroundColor')),
-					type: 'color',
-				},
-			)
-		},
-	],
+	plugins: [require("tailwindcss-animate")]
 } satisfies Config
