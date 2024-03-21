845bde229a4f8b96751955238e331cb5971032fd
Christian Leonardo <pleasefloyd73@gmail.com>
2024-03-21 13:48:10 -0300

    feat: add opacity 0 on hover screen size indicator

--- src/components/screen-size-indicator.tsx
+++ src/components/screen-size-indicator.tsx
@@ -4,7 +4,7 @@ export const ScreenSizeIndicator = () => {
 	if (process.env.NODE_ENV === "production") return null
 
 	return (
-		<div className="fixed bottom-2 left-2 z-50 flex items-center justify-center gap-2 rounded-full bg-foreground p-3 font-mono text-xs font-bold text-background">
+		<div className="fixed bottom-2 left-2 z-50 flex items-center justify-center gap-2 rounded-full bg-foreground p-3 font-mono text-xs font-bold text-background hover:opacity-0 transtion">
 			<div className="block sm:hidden">xs</div>
 			<div className="hidden sm:block md:hidden">sm</div>
 			<div className="hidden md:block lg:hidden">md</div>
