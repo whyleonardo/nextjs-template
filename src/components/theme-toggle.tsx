c862f84aacc45ab4fc9e757d330c3e95d5b973ef
Christian Leonardo <pleasefloyd73@gmail.com>
2024-03-27 09:44:10 -0300

    chore: adjust lucide icons

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from "next-themes"
 
-import { Icons } from "@/components/icons"
+import { Moon, Sun } from "lucide-react"
 
 export const ThemeToggle = () => {
 	const { setTheme, theme } = useTheme()
@@ -12,8 +12,8 @@ export const ThemeToggle = () => {
 			className="flex"
 			onClick={() => setTheme(theme === "light" ? "dark" : "light")}
 		>
-			<Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-			<Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+			<Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+			<Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 			<span className="sr-only">Toggle theme</span>
 		</button>
 	)
