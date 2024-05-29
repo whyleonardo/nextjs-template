880688bd406af5ecaa25d5140bfff677d2158629
Christian Leonardo <pleasefloyd73@gmail.com>
2024-05-29 17:47:06 -0300

    chore: add ui button

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,19 +2,22 @@
 
 import { useTheme } from "next-themes"
 
+import { Button } from "@/components/ui/button"
 import { Moon, Sun } from "lucide-react"
 
 export const ThemeToggle = () => {
 	const { setTheme, theme } = useTheme()
 
 	return (
-		<button
+		<Button
+			size="icon"
+			variant="ghost"
 			className="flex"
 			onClick={() => setTheme(theme === "light" ? "dark" : "light")}
 		>
 			<Sun className="dark:-rotate-90 rotate-0 scale-100 transition-all dark:scale-0" />
 			<Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 			<span className="sr-only">Toggle theme</span>
-		</button>
+		</Button>
 	)
 }
