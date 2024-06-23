5bb3a2a685598ef106ad777083c99d91482a7938
Christian Leonardo <pleasefloyd73@gmail.com>
2024-06-23 18:20:08 -0300

    refactor: update cal sans font var name

--- src/styles/fonts/index.ts
+++ src/styles/fonts/index.ts
@@ -3,11 +3,11 @@ import localFont from "next/font/local"
 import { GeistMono } from "geist/font/mono"
 import { GeistSans } from "geist/font/sans"
 
-const localCalSans = localFont({
+const calSans = localFont({
   src: "./cal-sans/CalSans-SemiBold.woff2",
   variable: "--font-cal-sans"
 })
 
-export const fontCalSans = localCalSans.variable
+export const fontCalSans = calSans.variable
 export const fontSans = GeistSans.variable
 export const fontMono = GeistMono.variable
