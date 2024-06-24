e9cf6300c577a084dfead9ed3a804507aba1d0a0
Christian Leonardo <pleasefloyd73@gmail.com>
2024-06-23 21:46:57 -0300

    chore: remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,39 +23,6 @@ export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`
-  },
-  description: siteConfig.description,
-  keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
-  authors: siteConfig.authors,
-  creator: "whyleonardo",
-  metadataBase: new URL(siteConfig.url),
-  openGraph: {
-    type: "website",
-    locale: "pt_BR",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name
-      }
-    ]
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: "@christianlsb0"
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png"
   }
 }
 
