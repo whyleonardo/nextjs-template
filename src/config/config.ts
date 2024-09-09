44594d39f8fdef4bb56c35bad87240ba8cc0ceca
Christian Leonardo <pleasefloyd73@gmail.com>
2024-09-09 12:42:10 -0300

    chore: some adjusts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import { env } from "@/lib/env.mjs"
+import { env } from "@/lib/env"
 
 export const siteConfig = {
   name: "Next Boilerplate",
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,12 +7,8 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SITE_BASE_URL: z.string().url().min(1)
   },
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+
   runtimeEnv: {
     NEXT_PUBLIC_SITE_BASE_URL: process.env.NEXT_PUBLIC_SITE_BASE_URL
   }
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
-  // experimental__runtimeEnv: {
-  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-  // }
 })
