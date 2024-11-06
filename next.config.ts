d1929ca220c2e957c82df394c57aac79a0b7d929
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-06 00:02:21 -0300

    chore: next.config now is ts file

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next/types"
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   logging: {
     fetches: {
