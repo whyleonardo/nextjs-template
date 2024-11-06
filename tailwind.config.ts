bdaf793e1d0e71347067a54eac3ab87b7162b041
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-06 11:03:55 -0300

    chore: update styles file and remove a unused code from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,12 @@
 import fluid, { extract, fontSize, screens } from "fluid-tailwind"
 import type { Config } from "tailwindcss"
 
-const svgToDataUri = require("mini-svg-data-uri")
-const {
-  default: flattenColorPalette
-} = require("tailwindcss/lib/util/flattenColorPalette")
-
 const config = {
   darkMode: ["class"],
   content: { files: ["./src/**/*.{ts,tsx}"], extract },
   theme: {
+    fontSize,
+    screens,
     container: {
       center: true,
       padding: "2rem",
@@ -18,8 +15,6 @@ const config = {
       }
     },
     extend: {
-      fontSize,
-      screens,
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -73,6 +68,13 @@ const config = {
         card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))"
+        },
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))"
         }
       },
       borderRadius: {
@@ -87,12 +89,20 @@ const config = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" }
+          from: {
+            height: "0"
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)"
+          }
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" }
+          from: {
+            height: "var(--radix-accordion-content-height)"
+          },
+          to: {
+            height: "0"
+          }
         }
       },
       animation: {
@@ -104,42 +114,8 @@ const config = {
   plugins: [
     require("tailwindcss-animate"),
     require("@tailwindcss/typography"),
-    addVariablesForColors,
-    fluid,
-    function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          "bg-grid": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          "bg-grid-small": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          "bg-dot": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`
-          })
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      )
-    }
+    fluid
   ]
 } satisfies Config
 
 export default config
-
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"))
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  )
-
-  addBase({
-    ":root": newVars
-  })
-}
