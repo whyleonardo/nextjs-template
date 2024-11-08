39afd7730ab7a4a346865ace23946cbe308201dd
Christian Leonardo <whyleonardo.dev@gmail.com>
2024-11-08 14:43:15 -0300

    chore: add new env examples

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,7 +3,13 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  server: {},
+  server: {
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().url(),
+    DATABASE_URL: z.string().url(),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1)
+  },
   client: {
     NEXT_PUBLIC_SITE_BASE_URL: z.string().url().optional()
   },
