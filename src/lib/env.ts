3b34b5ecd727dcda3bfbbeaf3e0330a811cdcc5e
Christian Leonardo <whyleonardo.dev@gmail.com>
2025-01-04 17:19:48 -0300

    chore: remove unused envs
    Signed-off-by: Christian Leonardo <whyleonardo.dev@gmail.com>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,13 +3,7 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  server: {
-    BETTER_AUTH_SECRET: z.string().min(1),
-    BETTER_AUTH_URL: z.string().url(),
-    DATABASE_URL: z.string().url(),
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1)
-  },
+  server: {},
   client: {
     NEXT_PUBLIC_SITE_BASE_URL: z.string().url().optional()
   },
